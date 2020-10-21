88ffdc7fb4b4f582ef274844da9c673dba4f43d2
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2020-10-21 20:08:54 -0300

    feat: Implement Person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Gabriel', age: 19 },
+      { name: 'Griselda', age: 10 },
+      { name: 'Mitchell', age: 30 }
+    ],
+    otherState: 'whatever'
+  }
+
+  switchNamesHandler = () => {
+    const persons = [...this.state.persons];
+    persons.reverse();
+    this.setState({persons});
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <h1>Hi! I'm a React App.</h1>
+        <button onClick={this.switchNamesHandler}>Switch names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Child Element</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
