cd2565fe8f6f1740afc4d8ac2f09d5ff68bcdc30
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2020-10-24 00:39:29 -0300

    refactor: Replace ternary operator for if

--- src/App.js
+++ src/App.js
@@ -67,18 +67,22 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNamesHandler} changed={this.nameChangedHandler} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNamesHandler} changed={this.nameChangedHandler}>Child Element</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNamesHandler} changed={this.nameChangedHandler} />
+        </div>);
+    }
+
     return (
       <div className="App">
         <h1>Hi! I'm a React App.</h1>
         <button style={style} onClick={this.togglePersonsHandler}>Switch names</button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNamesHandler} changed={this.nameChangedHandler} />
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNamesHandler} changed={this.nameChangedHandler}>Child Element</Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNamesHandler} changed={this.nameChangedHandler} />
-            </div> : null
-        }
+        {persons}
       </div>
     );
   }
