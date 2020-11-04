dc483f1a610ed2860c5ab4b0d8a01cf2c144cf7d
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2020-11-03 21:04:02 -0300

    feat: Extract Persons and Cockpit containers

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+    const appliedClasses = [];
+    let buttonClass = '';
+    if (props.showPersons) {
+        buttonClass = classes.Red;
+    }
+
+    if (props.persons.length <= 2) {
+        appliedClasses.push(classes.red);
+    }
+    if (props.persons.length <= 1) {
+        appliedClasses.push(classes.bold);
+    }
+    return (
+        <div className={classes.Cockpit}>
+            <h1>Hi! I'm a React App.</h1>
+            <p className={appliedClasses.join(' ')}>This is really working.</p>
+            <button className={buttonClass} onClick={props.clicked}>Toggle persons</button>
+        </div>
+    );
+};
+
+export default cockpit;
