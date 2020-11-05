9296a52784410f8f1ec0c14b0496cf2e9a63b901
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2020-11-04 22:04:31 -0300

    feat: Create withClass hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import classes from './Person.css';
+
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Fragment>
                 <p onClick={this.props.clicked}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} defaultValue={this.props.name} />
-            </ div>
+            </Fragment>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
